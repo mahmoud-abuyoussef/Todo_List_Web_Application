3751764842095028a0a0fcff674787be81e25207
Mahmoud Abuyoussef <mahmoudabuyoussef5@gmail.com>
2025-08-07 23:07:58 +0300

    feat(middleware): add middleware to protect routes based on auth token

---
+++ src/middleware.ts
@@ -0,0 +1,27 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+  const token = request.cookies.get("token")?.value;
+  console.log(token);
+  const isAuthPage = pathname.startsWith("/auth/login");
+  const isPublicAssets = ["/_next", "/favicon.ico", "/images"].some(prefix => pathname.startsWith(prefix));
+
+  if (isPublicAssets) {
+    return NextResponse.next();
+  }
+
+  if (isAuthPage && token) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  if (!isAuthPage && !token) {
+    return NextResponse.redirect(new URL("/auth/login", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
